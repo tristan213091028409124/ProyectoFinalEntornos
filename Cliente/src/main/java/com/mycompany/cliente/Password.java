2480966dfd9d3bd713ee83927ec2783e8036113d
Tristan-Alonso <atalonso89@ieszaidinvergeles.org>
2022-05-21 12:42:14 +0200

    Declarada variable password y implementado metodo compruebaPassword en clase Password

--- Cliente/src/main/java/com/mycompany/cliente/Password.java
+++ Cliente/src/main/java/com/mycompany/cliente/Password.java
@@ -9,5 +9,22 @@ package com.mycompany.cliente;
  * @author Tristan
  */
 public class Password {
+    String password;
     
+    public Password(String p){
+        this.password = p;
+    }
+    
+    public String compruebaPassword(String p){
+        if (p.length() < 8){
+            return "Debil";
+        }
+        if(p.matches("[a-zA-Z]+")){
+            return "Medio";
+        }
+        if(p.matches("[a-zA-Z0-9]+")){
+            return "Medio fuerte";
+        }
+        return "Fuerte";        
+    }
 }
